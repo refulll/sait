f327d8b9b6175fb7bf071c0890d77c89dad5c4b0
Cybersport <Cybersport@26kadr.internal>
2024-06-04 15:59:55 +0300

    dfdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Shapka from "./components/Shapka";
+import style1 from "./style/Style1.css"
+import monesy from "./foto/osnova2/monesy.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Shapka/>
+    <div className="block3">
+    <img className="bike3"src={monesy}></img>
     </div>
+  <div className="text2">
+    <p className="biketext3">Service, maintenance and insurance</p>
+    <p className="biketext4">The lease includes an all-in service package. Throughout the entire 36 months, you are fully insured against damage, theft and maintenance. In addition, you always have access to roadside assistance. </p>
+  </div>
+    </>
   );
 }
 
